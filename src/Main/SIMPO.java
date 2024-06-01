7959a3c04ca7e774e1238a684c486b537408f868
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-06-01 13:35:54 -0300

    INIT

---
+++ src/Main/SIMPO.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Main;
+
+/**
+ *
+ * @author guiespim
+ */
+public class SIMPO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new View.Login().setVisible(true);
+    }
+    
+}
